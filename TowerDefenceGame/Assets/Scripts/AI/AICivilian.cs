4723b916e914ac893cf6ee8adfca8e19820254d7
Freddie Babord <Freddie Babord>
2016-04-21 15:14:53 +0100

    Added more flexability to wave manager.  Created civilian AI

---
+++ TowerDefenceGame/Assets/Scripts/AI/AICivilian.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AICivilian : AIBase {
+
+    // The amount the civilian is worth
+    public int value = 2;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void ApplyDamage(float damageAmount)
+    {
+        base.ApplyDamage(damageAmount);
+    }
+}
