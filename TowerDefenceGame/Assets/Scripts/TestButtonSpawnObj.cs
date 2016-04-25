e4c9afbf87cbc55ca782b28e37e32aaa008fd7c4
Frederic Babord <dmfreddie@gmail.com>
2016-04-25 11:29:05 +0100

    Added dual analogue button input

---
+++ TowerDefenceGame/Assets/Scripts/TestButtonSpawnObj.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButtonSpawnObj : MonoBehaviour {
+
+    public GameObject temp;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Spawn()
+    {
+        Instantiate(temp, transform.position, transform.rotation);
+    }
+}
