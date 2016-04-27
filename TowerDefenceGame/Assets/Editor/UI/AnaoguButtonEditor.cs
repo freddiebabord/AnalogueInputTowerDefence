12bf4dae43693507e330c81f24b70b32016fee05
Freddie Babord <dmfreddie@gmail.com>
2016-04-27 23:11:16 +0100

    Updated analogue button

--- TowerDefenceGame/Assets/Editor/UI/AnaoguButtonEditor.cs
+++ TowerDefenceGame/Assets/Editor/UI/AnaoguButtonEditor.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/*
+    Script: AnalogueButtonEditor.cs
+    Author: Frederic Babord
+    This script is an extention of the Unity UI system that handles custom axis based
+    analogue inputs instead of the generic mouse click functionality. This shows the
+    defualt button inspector with the analogue axis string box.
+*/
+using UnityEngine;
 using UnityEditor;
 using UnityEngine.UI;
 using System.Collections;
@@ -7,7 +14,12 @@ using System.Collections;
 public class AnaoguButtonEditor : UnityEditor.UI.ButtonEditor {
 
 	public override void OnInspectorGUI() {
-        //AnalogueButtons component = (AnalogueButtons)target;
+        AnalogueButtons component = (AnalogueButtons)target;
+
+        component.clickAnalogueAxis = EditorGUILayout.TextField(
+            new GUIContent("Analogue Axis", "The input AXIS that acts as a click check"), 
+            component.clickAnalogueAxis
+        );
 
 		base.OnInspectorGUI();
 	}
