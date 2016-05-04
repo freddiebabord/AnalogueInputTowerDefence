d211fca5e0bc13e741cdce68ace1461aa3fe61aa
James Lamb <p4041543@tees.ac.uk>
2016-05-04 17:44:51 +0100

    Tower placement 2.0

--- TowerDefenceGame/Assets/Scripts/UI/TowerPlacement.cs
+++ TowerDefenceGame/Assets/Scripts/UI/TowerPlacement.cs
@@ -3,7 +3,16 @@ using System.Collections;
 
 public class TowerPlacement : MonoBehaviour {
 
+	enum Towers
+	{
+		IceTower = 0,
+		Magic,
+		ArrowTower,
+		Ballistics
+	};
+
 	Pointer point;
+	Towers index = Towers.IceTower;
 
 	void Start()
 	{
@@ -18,9 +27,17 @@ public class TowerPlacement : MonoBehaviour {
 
 			if(tile.GetComponent<NodePath>().pathType == NodePath.PathType.Grass && !tile.GetComponent<NodePath>().towerPlaced)
 			{
-				Instantiate (Resources.Load("Prefabs/Towers/AbsorbingTower"), tile.transform.position, tile.transform.rotation);
+				Instantiate (Resources.Load("Prefabs/Towers/"+index.ToString()), tile.transform.position, tile.transform.rotation);
 				tile.GetComponent<NodePath>().towerPlaced = true;
 			}
+		}
+
+		if (Input.GetAxis ("TriggerSelectLeft") >= 1) 
+		{
+			if(index > Towers.Ballistics)
+				index = Towers.IceTower;
+			else
+				index++;
 		}
 	}
 
