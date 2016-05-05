d5088eb9d1c6c1c17b812671a65417c12c3c2f21
James Lamb <p4041543@tees.ac.uk>
2016-05-05 15:16:54 +0100

    tower UI work

---
+++ TowerDefenceGame/Assets/Scripts/UI/ArrowUI.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowUI : MonoBehaviour {
+
+	Pointer point;
+	
+	TowerPlacement towers;
+	
+	GameObject tile;
+
+	bool arrow = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+		towers = GameObject.FindObjectOfType<TowerPlacement> ();
+		point = GameObject.FindObjectOfType<Pointer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (arrow && !towers.isTrue)
+			point.placeTower = false;
+		
+		if (point.placeTower) 
+		{
+			tile = point.currentTile;
+			//GameObject go = Instantiate (Resources.Load ("Prefabs/Towers/" + index.ToString ()), tile.transform.position, tile.transform.rotation) as GameObject;
+			
+			if (Input.GetAxis ("TriggerSelectRight") < 1) 
+			{
+				//Destroy(go);
+			}
+		} 
+		else 
+		{
+			tile = null;
+		}
+		
+		if (arrow && point.placeTower && Input.GetAxis ("TriggerSelectRight") >= 1) 
+		{
+			
+			if(tile.GetComponent<NodePath>().pathType == NodePath.PathType.Grass && !tile.GetComponent<NodePath>().towerPlaced)
+			{
+				Instantiate (Resources.Load("Prefabs/Towers/ArrowTower"), tile.transform.position, tile.transform.rotation);
+				tile.GetComponent<NodePath>().towerPlaced = true;
+			}
+		}
+	}
+	
+	public void PlaceArrow()
+	{
+		point.placeTower = !point.placeTower;
+		arrow = !arrow;
+	}
+}
---
+++ TowerDefenceGame/Assets/Scripts/UI/Ice.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ice : MonoBehaviour {
+
+	Pointer point;
+	
+	TowerPlacement towers;
+	
+	GameObject tile;
+
+	bool ice = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+		towers = GameObject.FindObjectOfType<TowerPlacement> ();
+		point = GameObject.FindObjectOfType<Pointer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (ice && !towers.isTrue)
+			point.placeTower = false;
+		
+		if (point.placeTower) 
+		{
+			tile = point.currentTile;
+			//GameObject go = Instantiate (Resources.Load ("Prefabs/Towers/" + index.ToString ()), tile.transform.position, tile.transform.rotation) as GameObject;
+			
+			if (Input.GetAxis ("TriggerSelectRight") < 1) 
+			{
+				//Destroy(go);
+			}
+		} 
+		else 
+		{
+			tile = null;
+		}
+		
+		if (ice && point.placeTower && Input.GetAxis ("TriggerSelectRight") >= 1) 
+		{
+			
+			if(tile.GetComponent<NodePath>().pathType == NodePath.PathType.Grass && !tile.GetComponent<NodePath>().towerPlaced)
+			{
+				Instantiate (Resources.Load("Prefabs/Towers/IceTower"), tile.transform.position, tile.transform.rotation);
+				tile.GetComponent<NodePath>().towerPlaced = true;
+			}
+		}
+	}
+	
+	public void PlaceIce()
+	{
+		point.placeTower = !point.placeTower;
+		ice = !ice;
+	}
+}
